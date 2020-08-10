61102f3185027ede5dc6cb703d58eab42dda2491
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-10 08:28:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,8 +28,8 @@ def get_english_meaning(string,emote)
 emoticons =load_library(string)
 emoticons.each do |(key,value)|
   value.each do |values|
-    if emote === values[0]
-      return values[1]
+    if emote === values[1]
+      return key
 end
 end
 end
