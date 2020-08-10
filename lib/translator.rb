87c32539d8c301073834af213e9304c2a4e6add7
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-10 08:22:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(string)
+lib = YAML.load_file(string)
+emoticons = {}
+lib.each do |(key,value)|
+  value.each_with_index do |emote,index|
+    if index != 1
+      emoticons[key] +=[{:english=>"#{emote}"}]
+    else
+      emoticons[key] +=[{:japanese=>"#{emote}"}]
+      binding.pry
+    end
+  end
+end
+return emoticons
 end
 
 def get_japanese_emoticon
